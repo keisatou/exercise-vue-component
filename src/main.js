e98ab476d42cb9a098846e943e069c74154ab1d5
keisatou <dummy>
2017-04-03 02:04:11 +0900

    split App.vue to some vue files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import Servers from './Servers.vue'
+
+Vue.component('app-header', Header);
+Vue.component('app-footer', Footer);
+Vue.component('app-servers', Servers);
 
 new Vue({
   el: '#app',
